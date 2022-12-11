08ed7358ac7886e73fe8df4553ff9abd1163bbc7
Ramez Taher <ramezgamer11@gmail.com>
2022-12-11 16:29:54 +0100

    auth & home page + routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from "react"
 import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import LogIn from "./pages/LogIn"
+import Register from "./pages/Register"
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>Hello home page</h1>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/register" element={<Register />} />
         <Route
           path="conversations"
           element={<h1>Conversations page</h1>}
---
+++ src/components/forms/RegisterForm.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const RegisterForm = () => {
+  return <form></form>
+}
+
+export default RegisterForm

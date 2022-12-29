544cb4d284811c46808cf25155b38e42969af3a1
Ramez Taher <ramezgamer11@gmail.com>
2022-12-29 21:30:12 +0100

    i swear gonna cry

--- src/pages/Channels.tsx
+++ src/pages/Channels.tsx
@@ -1,16 +1,16 @@
 import { Outlet, useParams } from "react-router-dom"
 
 import { Page } from "../utils/styles"
-// import mockChannels from "../mocks/mockChannels"
-// import ChatSidebar from "../components/chat/ChatSidebar"
-// import ChatDefault from "../components/chat/ChatDefault"
+import mockChannels from "../mocks/mockChannels"
+import ChatSidebar from "../components/chat/ChatSidebar"
+import ChatDefault from "../components/chat/ChatDefault"
 
 const Channels = () => {
   const { id } = useParams()
   return (
     <Page>
-      {/* <ChatSidebar channels={mockChannels} />
-      {!id && <ChatDefault />} */}
+      <ChatSidebar channels={mockChannels} />
+      {!id && <ChatDefault />}
       <Outlet />
     </Page>
   )

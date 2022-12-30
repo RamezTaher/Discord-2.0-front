ad3337afded54bbfafc23568730ce9cc1e61c2ff
Ramez Taher <ramezgamer11@gmail.com>
2022-12-30 11:26:41 +0100

    ahhhh

--- src/pages/Channels.tsx
+++ src/pages/Channels.tsx
@@ -2,8 +2,6 @@ import { Outlet, useParams } from "react-router-dom"
 
 import { Page } from "../utils/styles"
 import mockChannels from "../mocks/mockChannels"
-import ChatSidebar from "../components/chat/ChatSidebar"
-import ChatDefault from "../components/chat/ChatDefault"
 import LoginForm from "../components/forms/LoginForm"
 import RegisterForm from "../components/forms/RegisterForm"
 

efa37c076ae23b72d66f88794bf9999505becede
Ramez Taher <ramezgamer11@gmail.com>
2023-01-07 22:33:28 +0100

    return the channels page

--- src/pages/Channels.tsx
+++ src/pages/Channels.tsx
@@ -4,15 +4,16 @@ import { Page } from "../utils/styles"
 import mockChannels from "../mocks/mockChannels"
 import LoginForm from "../components/forms/LoginForm"
 import RegisterForm from "../components/forms/RegisterForm"
+import ChatSidebar from "../components/Chat/ChatSidebar"
+import ChatDefault from "../components/Chat/ChatDefault"
 
 const Channels = () => {
   const { id } = useParams()
   return (
     <Page>
-      {/* <ChatSidebar channels={mockChannels} />
-      {!id && <ChatDefault />} */}
-      <LoginForm />
-      <RegisterForm />
+      <ChatSidebar channels={mockChannels} />
+      {!id && <ChatDefault />}
+
       <Outlet />
     </Page>
   )

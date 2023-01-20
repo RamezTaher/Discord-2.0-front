ea8093728e197e182d1c1c7fb664ebf2d95ff534
Ramez Taher <ramezgamer11@gmail.com>
2023-01-20 22:56:06 +0100

    fixed the auth context

--- src/pages/Channel.tsx
+++ src/pages/Channel.tsx
@@ -1,10 +1,16 @@
-import React from "react"
+import React, { useContext } from "react"
 import { useParams } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 import { ChannelStyle } from "../utils/styles"
 
 const Channel = () => {
+  const { user } = useContext(AuthContext)
   const { id } = useParams()
-  return <ChannelStyle>the Conversation number {id}</ChannelStyle>
+  return (
+    <ChannelStyle>
+      {user?.email} && {user?.userName}
+    </ChannelStyle>
+  )
 }
 
 export default Channel

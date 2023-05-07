fe2cd39bf7e2ff006416124867d33f13ba11f12d
Ramez Taher <ramezgamer11@gmail.com>
2023-05-07 13:28:38 +0100

    add with crendentiale to the web socket

--- src/context/SocketContext.ts
+++ src/context/SocketContext.ts
@@ -1,5 +1,7 @@
 import { createContext } from "react"
 import { io } from "socket.io-client"
 
-export const socket = io(import.meta.env.VITE_WEBSOCKET_URL!)
+export const socket = io(import.meta.env.VITE_WEBSOCKET_URL!, {
+  withCredentials: true,
+})
 export const SocketContext = createContext(socket)
